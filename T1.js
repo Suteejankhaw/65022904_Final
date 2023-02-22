38b109761c2999575054fce4b56cd37ae65f38a9
Suteejankhaw <119989200+Suteejankhaw@users.noreply.github.com>
2023-02-22 15:05:27 +0700

    Final

---
+++ T1.js
@@ -0,0 +1,25 @@
+function submitForm() {
+    var name = document.getElementById("name").value
+    var surname = document.getElementById("surname").value
+    var birth = document.getElementById("birth").value
+    var gender = document.getElementById("gender").value
+    var result = document.getElementById("table")
+    if (gender && surname && birth && name) {
+        let R = document.createElement("tr")
+        result.appendChild(R)
+        let creaName = document.createElement("td")
+        let creaSurname = document.createElement("td")
+        let creaGrender = document.createElement("td")
+        let Birth_Date = document.createElement("td")
+
+        creaName.innerText = name
+        creaSurname.innerText = surname
+        creaGrender.innerText = gender
+        Birth_Date.innerText = birth
+
+        R.appendChild(creaName)
+        R.appendChild(creaSurname)
+        R.appendChild(creaGrender)
+        R.appendChild(Birth_Date)
+    }
+}
